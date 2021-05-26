adcf34994df115dc517e90d7c6139f1e41b2d481
Paul Marceau <paul@themarceaus.net>
2021-05-25 23:03:11 -0400

    Made it a PROMISE, to complete banner before next function call

--- includes/figlet.js
+++ includes/figlet.js
@@ -4,16 +4,19 @@ const colors = require("ansi-colors");
 //figlet used to create the employee database banner
 //ansi-colors ued to add color to the banner
 figletBanner = async () => {
-    figlet('Employee\nDatabase', function (err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.log(err);
-        }
-        console.log('*************************************************')
-        console.log(colors.bold.green(data))
-        console.log('*************************************************\n')
-        console.log("Starting the Employee DB, please hold.......");
-    });
+    return new Promise((resolve, reject) => {
+        figlet('Employee\nDatabase', function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                reject(err);
+            }
+            console.log('*************************************************')
+            console.log(colors.bold.green(data))
+            console.log('*************************************************\n')
+            console.log("Starting the Employee DB, please hold.......");
+            resolve(1)
+        })
+    })
 }
 
 module.exports = {
