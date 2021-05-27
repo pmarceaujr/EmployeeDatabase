fde36240a770d09414189db003d496429eb092f9
Paul Marceau <paul@themarceaus.net>
2021-05-26 23:18:14 -0400

    Added clear console command at the start of the application

--- includes/figlet.js
+++ includes/figlet.js
@@ -5,18 +5,19 @@ const colors = require("ansi-colors");
 //ansi-colors ued to add color to the banner
 figletBanner = async () => {
     return new Promise((resolve, reject) => {
-        figlet('Employee\nDatabase', function (err, data) {
+        figlet('EMPLOYEE\nDATABASE', function (err, data) {
             if (err) {
                 console.log('Something went wrong...');
                 reject(err);
             }
-
+            console.clear()
             console.log('')
             console.log('Welcome to the......')
             console.log('*************************************************')
             console.log(colors.bold.green(data))
             console.log('*************************************************\n')
             console.log("Starting the Employee DB, please hold.......");
+            console.log('')
             resolve(1)
         })
     })
