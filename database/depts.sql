672767fe86406a9049b5f0da853e2624185284a9
Paul Marceau <paul@themarceaus.net>
2021-05-26 21:42:02 -0400

    Added seed data files and DB schema files

---
+++ database/depts.sql
@@ -0,0 +1,11 @@
+insert into departments (dept_name)
+VALUES
+('Sales'),
+('Marketing'),
+('Engineering'),
+('Custmer Service'),
+('Finance'),
+('Accounting'),
+('Infomration Technology'),
+('Human Resources'),
+('Board Of Directors')
---
+++ database/schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+CREATE TABLE `departments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dept_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE departments AUTO_INCREMENT = 1
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `salary` decimal(10,0) NOT NULL,
+  `dept_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dept_id` (`dept_id`),
+  CONSTRAINT `role_ibfk_1` FOREIGN KEY (`dept_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=132 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE roles AUTO_INCREMENT = 1
+
+
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `role_id` int DEFAULT NULL,
+  `manager_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `manager_id` (`manager_id`),
+  CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`manager_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6417 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE employees AUTO_INCREMENT = 1001
