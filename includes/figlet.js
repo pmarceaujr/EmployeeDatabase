10e70b7f0244061e2e0c7bfce7dbe4bd21e87c08
Paul Marceau <paul@themarceaus.net>
2021-05-26 23:39:01 -0400

    New Banner message

--- includes/figlet.js
+++ includes/figlet.js
@@ -5,7 +5,7 @@ const colors = require("ansi-colors");
 //ansi-colors ued to add color to the banner
 figletBanner = async () => {
     return new Promise((resolve, reject) => {
-        figlet('EMPLOYEE\nDATABASE', function (err, data) {
+        figlet('Employee', function (err, data1) {
             if (err) {
                 console.log('Something went wrong...');
                 reject(err);
@@ -14,12 +14,22 @@ figletBanner = async () => {
             console.log('')
             console.log('Welcome to the......')
             console.log('*************************************************')
-            console.log(colors.bold.green(data))
-            console.log('*************************************************\n')
-            console.log("Starting the Employee DB, please hold.......");
-            console.log('')
-            resolve(1)
+            console.log(colors.bold.green(data1))
+            figlet('Database', function (err, data3) {
+                if (err) {
+                    console.log('Something went wrong...');
+                    reject(err);
+                }
+                console.log(colors.bold.green(data3))
+                //
+                console.log('*************************************************\n')
+                console.log("Starting the Employee DB, please hold.......");
+                console.log('')
+                resolve(1)
+            })
         })
+
+
     })
 }
 
